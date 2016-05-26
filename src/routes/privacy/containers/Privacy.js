db6162232a28a96ab011bb9bee34d9fcca841256
André Cruz <andremiguelcruz@msn.com>
2016-05-26 15:19:40 +0100

    Add privacy & terms.

--- src/routes/privacy/containers/Privacy.js
+++ src/routes/privacy/containers/Privacy.js
@@ -10,12 +10,155 @@ class Privacy extends Component {
         setTimeout(() => this.props.dispatch(unmarkAsLoading()), 300);
     }
 
+    shouldComponentUpdate() {
+        return false;
+    }
+
     render() {
         return (
             <div className="page page-privacy">
                 <Header appearance="menu-only" />
 
-                This will be the privacy page.
+                <div className="page-header">
+                    <div className="centered">Privacy Policy</div>
+                </div>
+
+                <div className="page-content">
+                    <strong>Last updated:</strong> April 27, 2016
+
+                    <p>
+                        This Privacy Policy governs the manner in which <span className="npms">npms</span> collects, uses, maintains and
+                        discloses information collected from users (each, a "User") of the <a href="https://npms.io">https://npms.io</a>
+                        website ("Site").
+                    </p>
+
+                    <h1>Personal identification information</h1>
+                    <p>
+                        We may collect personal identification information from Users in a variety of ways, including, but not limited to,
+                        when Users visit our site, fill out a form, and in connection with other activities, services, features or resources
+                        we make available on our Site. Users may visit our Site anonymously. We will collect personal identification
+                        information from Users only if they voluntarily submit such information to us. Users can always refuse to supply
+                        personally identification information, except that it may prevent them from engaging in certain Site related
+                        activities.
+                    </p>
+
+                    <h1>Non-personal identification information</h1>
+                    <p>
+                        We may collect non-personal identification information about Users whenever they interact with our Site.
+                        Non-personal identification information may include the browser name, the type of computer and technical information
+                        about Users means of connection to our Site, such as the operating system and the Internet service providers
+                        utilized and other similar information.
+                    </p>
+
+                    <h1>Web browser cookies</h1>
+                    <p>
+                        Our Site may use "cookies" to enhance User experience.
+                        User's web browser places cookies on their hard drive for record-keeping purposes and sometimes to track information
+                        about them. User may choose to set their web browser to refuse cookies, or to alert you when cookies are being sent.
+                        If they do so, note that some parts of the Site may not function properly.
+                    </p>
+
+                    <h1>How we use collected information</h1>
+                    <p>
+                        <span className="npms">npms</span> may collect and use Users personal information for the following purposes:
+
+                        <ul>
+                            <li>
+                                <strong>To run and operate our Site</strong>
+                                <p>We may need your information to display content on the Site correctly.</p>
+                            </li>
+                            <li>
+                                <strong>To improve customer service</strong>
+                                <p>Information you provide helps us respond to your customer service requests and support needs more
+                                efficiently.</p>
+                            </li>
+                            <li>
+                                <strong>To personalize user experience</strong>
+                                <p>We may use information in the aggregate to understand how our Users as a group use the services and
+                                resources provided on our Site.</p>
+                            </li>
+                            <li>
+                                <strong>To improve our Site</strong>
+                                <p>We may use feedback you provide to improve our products and services.</p>
+                            </li>
+                        </ul>
+                    </p>
+
+                    <h1>How we protect your information</h1>
+                    <p>
+                        We adopt appropriate data collection, storage and processing practices and security measures to protect against
+                        unauthorized access, alteration, disclosure or destruction of your personal information, username, password,
+                        transaction information and data stored on our Site.
+                    </p>
+
+                    <h1>Sharing your personal information</h1>
+                    <p>
+                        We do not sell, trade, or rent Users personal identification information to others. We may share generic aggregated
+                        demographic information not linked to any personal identification information regarding visitors and users with our
+                        business partners, trusted affiliates and advertisers for the purposes outlined above.
+                    </p>
+
+                    <h1>Third party websites</h1>
+                    <p>
+                        Users may find advertising or other content on our Site that link to the sites and services of our partners,
+                        suppliers, advertisers, sponsors, licensors and other third parties. We do not control the content or links that
+                        appear on these sites and are not responsible for the practices employed by websites linked to or from our Site.
+                        In addition, these sites or services, including their content and links, may be constantly changing. These sites and
+                        services may have their own privacy policies and customer service policies. Browsing and interaction on any other
+                        website, including websites which have a link to our Site, is subject to that website's own terms and policies.
+                    </p>
+
+                    <h1>Advertising</h1>
+                    <p>
+                        Ads appearing on our site may be delivered to Users by advertising partners, who may set cookies. These cookies
+                        allow the ad server to recognize your computer each time they send you an online advertisement to compile non
+                        personal identification information about you or others who use your computer. This information allows ad networks
+                        to, among other things, deliver targeted advertisements that they believe will be of most interest to you.
+                        This privacy policy does not cover the use of cookies by any advertisers.
+                    </p>
+
+                    <h1>Compliance with children's online privacy protection act</h1>
+                    <p>
+                        Protecting the privacy of the very young is especially important. For that reason, we never collect or maintain
+                        information at our Site from those we actually know are under 13, and no part of our website is structured to
+                        attract anyone under 13.
+                    </p>
+
+                    <h1>Advertising</h1>
+                    <p>
+                        Ads appearing on our site may be delivered to Users by advertising partners, who may set cookies. These cookies
+                        allow the ad server to recognize your computer each time they send you an online advertisement to compile non
+                        personal identification information about you or others who use your computer. This information allows ad networks
+                        to, among other things, deliver targeted advertisements that they believe will be of most interest to you.
+                        This privacy policy does not cover the use of cookies by any advertisers.
+                    </p>
+
+                    <h1>Your acceptance of these terms</h1>
+                    <p>
+                        By using this Site, you signify your acceptance of this policy. If you do not agree to this policy, please do not
+                        use our Site. Your continued use of the Site following the posting of changes to this policy will be deemed your
+                        acceptance of those changes.
+                    </p>
+
+                    <h1>Changes to this privacy policy</h1>
+                    <p>
+                        <span className="npms">npms</span> has the discretion to update this privacy policy at any time. When we do, we will
+                        post a notification on the main page of our Site and revise the updated date at the top of this page. We encourage
+                        Users to frequently check this page for any changes to stay informed about how we are helping to protect the
+                        personal information we collect. You acknowledge and agree that it is your responsibility to review this privacy
+                        policy periodically and become aware of modifications.
+                    </p>
+                    <p>
+                        By continuing to access or use our Service after those changes become effective, you agree to be bound by the
+                        revised terms. If you do not agree to the new policy, please stop using the Service.
+                    </p>
+
+                    <h1>Contacting us</h1>
+                    <p>
+                        If you have any questions about this Privacy Policy, the practices of this site, or your dealings with this site,
+                        please contact us.
+                    </p>
+                </div>
             </div>
         );
     }
--- src/routes/terms/containers/Terms.js
+++ src/routes/terms/containers/Terms.js
@@ -10,12 +10,94 @@ class Terms extends Component {
         setTimeout(() => this.props.dispatch(unmarkAsLoading()), 300);
     }
 
+    shouldComponentUpdate() {
+        return false;
+    }
+
     render() {
         return (
             <div className="page page-terms">
                 <Header appearance="menu-only" />
 
-                This will be the terms page.
+                <div className="page-header">
+                    <div className="centered">Terms of Service</div>
+                </div>
+
+                <div className="page-content">
+                    <strong>Last updated:</strong> April 27, 2016
+
+                    <p>
+                        Please read these Terms of Service ("Terms", "Terms of Service") carefully before using
+                        the <a href="https://npms.io">https://npms.io</a> website (the "Service") operated by
+                        <span className="npms">npms</span> ("us", "we", or "our").
+                    </p>
+                    <p>
+                        Your access to and use of the Service is conditioned on your acceptance of and compliance with these Terms.
+                        These Terms apply to all visitors, users and others who access or use the Service.
+                    </p>
+                    <p>
+                        By accessing or using the Service you agree to be bound by these Terms. If you disagree with any part of the terms
+                        then you may not access the Service.
+                    </p>
+
+                    <h1>Links To Other Web Sites</h1>
+                    <p>
+                        Our Service may contain links to third-party web sites or services that are not owned or controlled by
+                        <span className="npms">npms</span>.
+                    </p>
+                    <p>
+                        <span className="npms">npms</span> has no control over, and assumes no responsibility for, the content, privacy
+                        policies, or practices of any third party web sites or services. You further acknowledge and agree that
+                        <span className="npms">npms</span> shall not be responsible or liable, directly or indirectly, for any damage or
+                        loss caused or alleged to be caused by or in connection with use of or reliance on any such content, goods or
+                        services available on or through any such web sites or services.
+                    </p>
+                    <p>
+                        We strongly advise you to read the terms and conditions and privacy policies of any third-party web sites or
+                        services that you visit.
+                    </p>
+
+                    <h1>Termination</h1>
+                    <p>
+                        We may terminate or suspend access to our Service immediately, without prior notice or liability, for any reason
+                        whatsoever, including without limitation if you breach the Terms.
+                    </p>
+                    <p>
+                        All provisions of the Terms which by their nature should survive termination shall survive termination, including,
+                        without limitation, ownership provisions, warranty disclaimers, indemnity and limitations of liability.
+                    </p>
+
+                    <h1>Governing Law</h1>
+                    <p>
+                        These Terms shall be governed and construed in accordance with the laws of Portugal, without regard to its conflict
+                        of law provisions.
+                    </p>
+                    <p>
+                        Our failure to enforce any right or provision of these Terms will not be considered a waiver of those rights.
+                        If any provision of these Terms is held to be invalid or unenforceable by a court, the remaining provisions of these
+                        Terms will remain in effect. These Terms constitute the entire agreement between us regarding our Service, and
+                        supersede and replace any prior agreements we might have between us regarding the Service.
+                    </p>
+
+                    <h1>Changes to this terms of service</h1>
+                    <p>
+                        <span className="npms">npms</span> has the discretion to update this terms of service at any time. When we do, we
+                        will post a notification on the main page of our Site and revise the updated date at the top of this page.
+                        We encourage Users to frequently check this page for any changes to stay informed about service terms.
+                        You acknowledge and agree that it is your responsibility to review this terms of service periodically and become
+                        aware of modifications.
+                    </p>
+                    <p>
+                        By continuing to access or use our Service after those changes become effective, you agree to be bound by the
+                        revised terms. If you do not agree to the new terms, please stop using the Service.
+                    </p>
+
+                    <h1>Contacting us</h1>
+                    <p>
+                        If you have any questions about this Terms of Service, the practices of this site, or your dealings with this site,
+                        please contact us.
+                    </p>
+                </div>
             </div>
         );
     }
