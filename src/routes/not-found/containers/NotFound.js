c049311edb17b6940c335601b65d129176c96273
André Cruz <andremiguelcruz@msn.com>
2016-05-30 23:43:57 +0100

    404 page.

--- src/routes/not-found/containers/NotFound.js
+++ src/routes/not-found/containers/NotFound.js
@@ -1,5 +1,6 @@
 import './NotFound.css';
 import React, { Component, PropTypes } from 'react';
+import { Link } from 'react-router';
 import { connect } from 'react-redux';
 import Header from 'shared/containers/header/Header';
 
@@ -9,7 +10,12 @@ class NotFound extends Component {
             <div className="page page-not-found">
                 <Header />
 
-                This will be the 404 page.
+                <div className="page-header">404</div>
+
+                <div className="page-content">
+                    <div className="headline">Oops, can't find what you are looking for.</div>
+                    <p>The page you requested does not exist, go back to <Link to={ '/' }>home</Link>?</p>
+                </div>
             </div>
         );
     }
