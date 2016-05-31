ad91b04f73277bc0a7ae8c52db6c9a70979168f6
André Cruz <andremiguelcruz@msn.com>
2016-05-31 15:18:19 +0100

    Remove weird hack, it's not fixed.

--- src/polyfills.js
+++ src/polyfills.js
@@ -1,6 +1,6 @@
 import 'dom4';
 import Promise from 'bluebird';
-import { polyfill as smoothscroll } from 'smoothscroll-polyfill/src/smoothscroll';
+import { polyfill as smoothscroll } from 'smoothscroll-polyfill';
 
 smoothscroll();
 window.Promise = Promise;
