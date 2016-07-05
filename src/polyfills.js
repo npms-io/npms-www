f330232ed5cd1384f1508bc62ac389795c7904d2
Andre Cruz <andremiguelcruz@msn.com>
2016-07-05 17:18:07 +0100

    Configure bluebird.

--- src/polyfills.js
+++ src/polyfills.js
@@ -2,5 +2,11 @@ import 'dom4';
 import Promise from 'bluebird';
 import { polyfill as smoothscroll } from 'smoothscroll-polyfill';
 
+// Smooth scroll polyfill
 smoothscroll();
+
+// Make bluebird global
 window.Promise = Promise;
+
+// Improve debugging by enabling long stack traces.. it has minimal impact in production
+Promise.config({ longStackTraces: true, warnings: false });
