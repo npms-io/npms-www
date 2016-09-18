423a99408f3baba9026cac22cf60cdcd6aea1b26
Andre Cruz <andremiguelcruz@msn.com>
2016-09-18 11:48:29 +0100

    Small change of flags.

--- src/shared/containers/search-box/SearchBoxHelp.js
+++ src/shared/containers/search-box/SearchBoxHelp.js
@@ -18,7 +18,7 @@ export default class SearchBoxHelp extends Component {
                     <li><code>author:sindresorhus</code> Show or filter results by author</li>
                     <li><code>maintainer:sindresorhus</code> Show or filter results by maintainer</li>
                     <li><code>keywords:gulpplugin</code> Show or filter results by keywords</li>
-                    <li><code>exclude:deprecated</code> Exclude packages that are deprecated, unstable or insecure</li>
+                    <li><code>not:deprecated</code> Exclude packages that are deprecated, unstable or insecure</li>
                 </ul>
 
                 <div>For the complete list of qualifiers, please read the Search API <a href="https://api-docs.npms.io" target="_blank">documentation</a>.</div>
