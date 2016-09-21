f38ab411a600468a870a69eec657216091e8a046
Andre Cruz <andremiguelcruz@msn.com>
2016-09-21 19:51:46 +0100

    Fix left & top edges for tooltips.

--- src/shared/components/tooltip/util/checkEdges.js
+++ src/shared/components/tooltip/util/checkEdges.js
@@ -2,13 +2,13 @@ export default function checkEdges(tooltipEl) {
     const tooltipBounds = tooltipEl.getBoundingClientRect();
     const bodyBounds = document.body.getBoundingClientRect();
 
-    if (tooltipBounds.left === 0) {
+    if (tooltipBounds.left < 10) {
         tooltipEl.style.left = `${parseInt(tooltipEl.style.left, 10) + 10}px`;
     } else if (tooltipBounds.left + tooltipBounds.width > bodyBounds.width - 10) {
         tooltipEl.style.left = `${parseInt(tooltipEl.style.left, 10) - 10}px`;
     }
 
-    if (tooltipBounds.top === 0) {
+    if (tooltipBounds.top < 10) {
         tooltipEl.style.top = `${parseInt(tooltipEl.style.top, 10) + 10}px`;
     } else if (tooltipBounds.top + tooltipBounds.height > bodyBounds.height - 10) {
         tooltipEl.style.top = `${parseInt(tooltipEl.style.top, 10) - 10}px`;
