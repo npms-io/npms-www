83d8d98b400945462896a01e16a3b02f9412b7ac
Andre Cruz <andremiguelcruz@msn.com>
2016-09-21 20:25:48 +0100

    Fix last commit.

--- src/shared/components/tooltip/util/checkEdges.js
+++ src/shared/components/tooltip/util/checkEdges.js
@@ -1,16 +1,20 @@
 export default function checkEdges(tooltipEl) {
-    const tooltipBounds = tooltipEl.getBoundingClientRect();
     const bodyBounds = document.body.getBoundingClientRect();
+    const bounds = tooltipEl.getBoundingClientRect();
+    const cssPosition = {
+        top: parseFloat(tooltipEl.style.top),
+        left: parseFloat(tooltipEl.style.left),
+    };
 
-    if (tooltipBounds.left < 10) {
-        tooltipEl.style.left = `${parseInt(tooltipEl.style.left, 10) + 10}px`;
-    } else if (tooltipBounds.left + tooltipBounds.width > bodyBounds.width - 10) {
-        tooltipEl.style.left = `${parseInt(tooltipEl.style.left, 10) - 10}px`;
+    if (bounds.left < 10) {
+        tooltipEl.style.left = `${cssPosition.left + 10 - bounds.left}px`;
+    } else if (bounds.left + bounds.width > bodyBounds.width - 10) {
+        tooltipEl.style.left = `${cssPosition.left - 10 + (bodyBounds.width - bounds.left - bounds.width)}px`;
     }
 
-    if (tooltipBounds.top < 10) {
-        tooltipEl.style.top = `${parseInt(tooltipEl.style.top, 10) + 10}px`;
-    } else if (tooltipBounds.top + tooltipBounds.height > bodyBounds.height - 10) {
-        tooltipEl.style.top = `${parseInt(tooltipEl.style.top, 10) - 10}px`;
+    if (bounds.top < 10) {
+        tooltipEl.style.top = `${cssPosition.top + 10 - bounds.top}px`;
+    } else if (bounds.top + bounds.height > bodyBounds.height - 10) {
+        tooltipEl.style.top = `${cssPosition.top - 10 + (bounds.top + bounds.height - bodyBounds.height)}px`;
     }
 }
