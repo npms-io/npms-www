6af8b319234801f44351d4fa7cf1d221e777d5b2
Andre Cruz <andremiguelcruz@msn.com>
2016-10-29 22:04:05 +0100

    Update deps and fix build.

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,8 +1,4 @@
-import { addReducers } from 'shared/state/store';
 import Home from './containers/Home';
-import reducers from './state/reducers';
-
-addReducers(reducers);
 
 export default {
     component: Home,
